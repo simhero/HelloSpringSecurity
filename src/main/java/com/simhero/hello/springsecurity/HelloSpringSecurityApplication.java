05ec7b8d8c338b4204248d6f4071cb8e2646da71
goodman <simhero@gmai.com>
2021-02-04 21:31:43 +0900

    init

---
+++ src/main/java/com/simhero/hello/springsecurity/HelloSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.simhero.hello.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simhero/hello/springsecurity/HelloSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simhero.hello.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
